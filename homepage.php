0908770875c299e542604134d387645499600c0a
Erroll <cues1707@gmail.com>
2025-12-28 16:34:02 +0530

    index

--- homepage.php
+++ homepage.php
@@ -5,6 +5,15 @@ require "db_pdo.php";
 require "data.php";
 
 
-$place = $_GET['place_id'];
-echo $place;
-return;
+// $place = $_GET['place_id'];
+// echo $place;
+// return;
+
+ $this->query("SELECT * from Places WHERE ID = ?");
+        $this->bind(1, $place_id);
+
+        $row_place = $this->single();
+        $count = $this->count();
+
+        echo $count;
+        return;
--- index.php
+++ index.php
@@ -5,18 +5,23 @@ require "db_pdo.php";
 require "data.php";
 
 
-switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
-    case '/':
-        require 'homepage.php';
-        break;
-    case '/contact.php':
-        require 'contact.php';
-        break;
-    case '/API/Articles':
-        require __DIR__ . '/API/Articles/index.php';
-        break;
-    default:
-        http_response_code(404);
-        exit('Not Found');
-}
+$place = $_GET['place_id'];
+echo $place;
+return;
+
+
+// switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
+//     case '/':
+//         require 'homepage.php';
+//         break;
+//     case '/contact.php':
+//         require 'contact.php';
+//         break;
+//     case '/API/Articles':
+//         require __DIR__ . '/API/Articles/index.php';
+//         break;
+//     default:
+//         http_response_code(404);
+//         exit('Not Found');
+// }
 
