b2b1e4956c7559ae746157d9b8260b8b4e6c9e8a
Erroll <cues1707@gmail.com>
2025-12-28 16:12:19 +0530

    index

--- index.php
+++ index.php
@@ -7,18 +7,16 @@ require "data.php";
 
 switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
     case '/':
-        require 'index.php';
-        break;
-    case '/API/Articles':
-        require __DIR__'API/Articles/index.php';
-        break;
-    case '/homepage':
         require 'homepage.php';
         break;
     case '/contact.php':
         require 'contact.php';
         break;
+    case '/API/Articles':
+        require __DIR__ . '/API/Articles/index.php';
+        break;
     default:
         http_response_code(404);
         exit('Not Found');
 }
+
