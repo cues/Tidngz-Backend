0f8c95f8c57bdaf617338762cc9fe5d804f6cd90
Erroll <cues1707@gmail.com>
2025-12-28 16:05:45 +0530

    index

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ require "data.php";
 
 
 switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
+     case '/API/Articles':
+        require __DIR__'API/Articles/index.php';
+        break;
     case '/homepage.php':
         require 'homepage.php';
         break;
